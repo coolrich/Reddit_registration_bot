fb693984bb5a4da18b7956d6ab0336b0fec7ff13
ihor <i.domnich2017@gmail.com>
2023-07-24 15:14:06 +0300

    Refactoring. Creating a class.

--- main.py
+++ main.py
@@ -6,87 +6,97 @@ from selenium.webdriver import Keys
 from selenium_recaptcha_solver import RecaptchaSolver
 from selenium_recaptcha_solver.exceptions import RecaptchaException
 
-test_ua = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
-reg_url = "https://www.reddit.com/account/register/?experiment_d2x_2020ify_buttons=enabled&use_accountmanager=true&experiment_d2x_google_sso_gis_parity=enabled&experiment_d2x_onboarding=enabled&experiment_d2x_am_modal_design_update=enabled"
-login = "craftsman94.test@gmail.com"
-password = "some_password"
 
-def go_to_reddit():
-    global chrome
-    chrome = get_driver()
-    chrome.implicitly_wait(0.5)
-    chrome.get(reg_url)
+class SignUpForReddit:
+    def __init__(self):
+        self.user_agent_headers = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
+        self.reg_url = "https://www.reddit.com/account/register/?experiment_d2x_2020ify_buttons=enabled&use_accountmanager=true&experiment_d2x_google_sso_gis_parity=enabled&experiment_d2x_onboarding=enabled&experiment_d2x_am_modal_design_update=enabled"
+        self.login = "craftsman94.test@gmail.com"
+        self.password = "some_password"
+        self.reg_username = None
+        self.reg_field = None
+        self.continue_button = None
+        chrome_opts = self.__init_chrome_opts()
+        self.chrome = webdriver.Chrome(options=chrome_opts)
 
-# Get Chrome driver
-def get_driver():
-    global chrome
-    chrome_opts = Options()
-    chrome_opts.add_argument(f'--user-agent={test_ua}')
-    chrome_opts.add_argument("--disable-notifications")
-    return webdriver.Chrome(options=chrome_opts)
+    def __init_chrome_opts(self) -> Options:
+        chrome_opts = Options()
+        chrome_opts.add_argument(f'--user-agent={self.user_agent_headers}')
+        chrome_opts.add_argument("--disable-notifications")
+        return chrome_opts
+
+    def __go_to_reddit(self) -> None:
+        # global chrome
+        self.chrome.implicitly_wait(0.5)
+        self.chrome.get(self.reg_url)
+
+    def __printing_email(self):
+        email_field = self.chrome.find_element(by=By.ID, value="regEmail")
+        for char in self.login:
+            email_field.send_keys(char)
+            self.__wait(0.1)
+        email_field.send_keys(Keys.ENTER)
+
+    def __save_value_from_reg_name(self):
+        self.reg_username = self.chrome.find_element(by=By.ID, value="regUsername")
+
+    def __get_reg_field(self):
+        self.reg_field = self.chrome.find_element(by=By.ID, value="regPassword")
+
+    def __printing_password(self):
+        for char in self.password:
+            self.reg_field.send_keys(char)
+            self.__wait(0.1)
+
+    def __submit_regfield(self):
+        self.reg_field.send_keys(Keys.ENTER)
+
+    def __solve_captcha(self):
+        solver = RecaptchaSolver(driver=self.chrome)
+        recaptcha_iframe = self.chrome.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]')
+        is_repeat = True
+        while is_repeat:
+            try:
+                self.__wait(3)
+                solver.click_recaptcha_v2(iframe=recaptcha_iframe)
+                is_repeat = False
+            except RecaptchaException:
+                print(RecaptchaException)
+                self.__wait(3)
+        self.__wait(3)
+
+    def __get_cont_butt(self):
+        self.continue_button = self.chrome.find_element(by=By.CSS_SELECTOR, value="button.SignupButton")
+
+    def __click_continue(self):
+        self.continue_button.click()
+
+    def __quit_browser(self):
+        self.chrome.quit()
+
+    @staticmethod
+    def __wait(seconds):
+        sleep(seconds)
+
+    def execute(self):
+        self.__go_to_reddit()
+        input("Enter any key to continue...")
+        self.__printing_email()
+        self.__wait(2)
+        self.__save_value_from_reg_name()
+        self.__get_reg_field()
+        self.__printing_password()
+        self.__wait(2)
+        self.__submit_regfield()
+        self.__wait(2)
+        self.__get_cont_butt()
+        self.__click_continue()
+        # input("any key to continue...")
+        self.__wait(2)
+        self.__solve_captcha()
+        # self.click_continue()
+        input("Press any key to close the program...")
+        self.__quit_browser()
 
 
-def typing_email():
-    email_field = chrome.find_element(by=By.ID, value="regEmail")
-    for char in login:
-        email_field.send_keys(char)
-        sleep(0.1)
-    email_field.send_keys(Keys.ENTER)
-
-
-def get_regname():
-    global reg_username
-    reg_username = chrome.find_element(by=By.ID, value="regUsername")
-
-
-def get_regfield():
-    global reg_field
-    reg_field = chrome.find_element(by=By.ID, value="regPassword")
-
-
-def typing_password():
-    for char in password:
-        reg_field.send_keys(char)
-        sleep(0.1)
-
-def press_enter():
-    reg_field.send_keys(Keys.ENTER)
-
-def solve_captcha():
-    solver = RecaptchaSolver(driver=chrome)
-    recaptcha_iframe = chrome.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]')
-    is_repeat = True
-    while is_repeat:
-        try:
-            sleep(3)
-            solver.click_recaptcha_v2(iframe=recaptcha_iframe)
-            is_repeat = False
-        except RecaptchaException:
-            print(RecaptchaException)
-            sleep(3)
-    sleep(3)
-
-def click_continue():
-    global continue_button
-    continue_button = chrome.find_element(by=By.CSS_SELECTOR, value="button.SignupButton")
-    continue_button.click()
-
-
-
-
-go_to_reddit()
-typing_email()
-sleep(2)
-get_regname()
-get_regfield()
-typing_password()
-sleep(2)
-press_enter()
-sleep(2)
-click_continue()
-# input("any key to continue...")
-sleep(2)
-solve_captcha()
-click_continue()
-input("Press any key to close the program...")
-chrome.quit()
+SignUpForReddit().execute()
