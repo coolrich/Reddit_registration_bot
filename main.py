361e85e09eb170e87d4c8bec5611b5fa97f36208
ihor <i.domnich2017@gmail.com>
2023-07-26 20:15:22 +0300

    A lot of changes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pprint
-import sys
 from time import sleep
 from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import ElementNotVisibleException
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Keys
@@ -17,33 +16,51 @@ from selenium_recaptcha_solver.exceptions import RecaptchaException
 
 # from selenium.webdriver.common.proxy import Proxy, ProxyType
 
+class Data:
+    """Stores the usernames, emails, passwords, and API keys"""
+    def __init__(self):
+        self.email_accounts = []
+        self.reddit_accounts = []
+
+    def __create(self):
+        pass
+
+
+class CreateSkiffEmails:
+    pass
+
 
 class SignUpForReddit:
-    def __init__(self, use_proxy: bool):
+    def __init__(self, email: str = "craftsman94.test@gmail.com",
+                 password: str = "some_password",
+                 is_detached: bool = True,
+                 use_proxy: bool = True, ):
         self.is_re_test = False
-
-        self.__proxy_list = ["39.59.1.14:8080", "118.31.112.32:80"]
         self.__use_proxy = use_proxy
-        self.__user_agent_headers = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                                    'Chrome/37.0.2049.0 Safari/537.36'
         self.__reg_url = "https://www.reddit.com/account/register/?experiment_d2x_2020ify_buttons=enabled" \
                          "&use_accountmanager=true&experiment_d2x_google_sso_gis_parity=enabled" \
                          "&experiment_d2x_onboarding=enabled&experiment_d2x_am_modal_design_update=enabled"
-        self.__email = "craftsman94.test@gmail.com"
-        self.__password = "some_password"
+        self.__user_agent_headers = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                                    'Chrome/37.0.2049.0 Safari/537.36'
+
+        self.__proxy_list = ["39.59.1.14:8080", "118.31.112.32:80", "51.124.209.11:80", "109.254.62.194:9090"]
+        self.__used_proxies_list = []
+        self.__email = email
+        self.__password = password
         self.__reg_username = None
         self.__reg_field = None
         self.__continue_button = None
-        chrome_opts = self.__init_chrome_opts()
-        # capabilities = self.__get_capabilities_with_proxy()
+        chrome_opts = self.__init_chrome_opts(is_detached)
         self.__chrome = webdriver.Chrome(options=chrome_opts)
-        # self.__check_for_use_proxy()
 
-    def __init_chrome_opts(self) -> Options:
+    def __init_chrome_opts(self, is_detached: bool = False) -> Options:
         chrome_opts = Options()
         chrome_opts.add_argument(f'--user-agent={self.__user_agent_headers}')
         chrome_opts.add_argument("--disable-notifications")
-        chrome_opts.add_experimental_option("detach", True)
+        chrome_opts.add_experimental_option("detach", is_detached)
+        # chrome_opts.add_argument("--headless")
+        # chrome_opts.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # chrome_opts.add_experimental_option('useAutomationExtension', False)
         return chrome_opts
 
     def __go_to_reddit(self) -> None:
@@ -62,7 +79,7 @@ class SignUpForReddit:
             # self.__check_for_use_proxy_and_restart()
 
     def __save_value_from_reg_name(self):
-        self.__reg_username = self.__chrome.find_element(by=By.ID, value="regUsername")
+        self.__reg_username = self.__chrome.find_element(by=By.ID, value="regUsername").get_attribute("value")
 
     def __get_reg_field(self):
         self.__reg_field = self.__chrome.find_element(by=By.ID, value="regPassword")
@@ -80,14 +97,14 @@ class SignUpForReddit:
             solver = RecaptchaSolver(driver=self.__chrome)
             recaptcha_iframe = WebDriverWait(self.__chrome, 3).until(
                 EC.visibility_of_element_located((By.XPATH, '//iframe[@title="reCAPTCHA"]')))
-            self.recaptcha_exception_test()
+            self.__recaptcha_exception_test()
             solver.click_recaptcha_v2(iframe=recaptcha_iframe)
         except RecaptchaException as rec_exc:
             print(rec_exc)
         except StaleElementReferenceException as sere:
             print(sere)
 
-    def recaptcha_exception_test(self):
+    def __recaptcha_exception_test(self):
         if self.is_re_test:
             self.is_re_test = False
             raise RecaptchaException()
@@ -96,7 +113,12 @@ class SignUpForReddit:
         self.__continue_button = self.__chrome.find_element(by=By.CSS_SELECTOR, value="button.SignupButton")
 
     def __click_continue(self):
-        self.__continue_button.click()
+        try:
+            self.__continue_button.click()
+        except StaleElementReferenceException as sere:
+            print(sere)
+        except ElementClickInterceptedException as ecie:
+            print(ecie)
 
     def __quit_browser(self):
         self.__chrome.quit()
@@ -117,7 +139,8 @@ class SignUpForReddit:
     def __set_new_proxy(self):
         # new_proxy_opt = Options().add_argument(f"--proxy-server={self.proxy_list.pop()}")
         if len(self.__proxy_list) != 0:
-            proxy = f"{self.__proxy_list.pop()}"
+            proxy = self.__proxy_list.pop()
+            self.__used_proxies_list.append(proxy)
             self.__chrome.capabilities['proxy'] = {
                 "httpProxy": proxy,
                 "ftpProxy": proxy,
@@ -126,7 +149,8 @@ class SignUpForReddit:
             }
             print("Proxy changed to ", self.__chrome.capabilities['proxy'])
         else:
-            print("There is no proxy left!")
+            print("There is no proxy left! Start from begin.")
+            self.__proxy_list.extend(self.__used_proxies_list)
 
     def __restart_webdriver(self):
         self.__reg_username = None
@@ -142,7 +166,7 @@ class SignUpForReddit:
             print(toe)
             self.__check_for_use_proxy_and_restart()
 
-    def __execute(self):
+    def execute(self):
         # input("Enter any key to continue...")
         self.__go_to_reddit()
         self.__printing_email()
@@ -168,10 +192,14 @@ class SignUpForReddit:
             'API_key': None
         }
 
-    @staticmethod
-    def create_accounts(number_of_acc: int):
-        for i in range(1, number_of_acc + 1):
-            acc_details = SignUpForReddit(True).__execute()
-            pprint.pp(acc_details)
-    input("Done. Press enter to close the program...")
 
+class RedditAccountsFactory:
+    @staticmethod
+    def create_accounts(number_of_acc: int = 1):
+        for i in range(1, number_of_acc + 1):
+            acc_details = SignUpForReddit().execute()
+            pprint.pp(acc_details)
+        input("Done. Press enter to close the program...")
+
+
+RedditAccountsFactory.create_accounts()
