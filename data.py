9edfaea4dfc35b410d52bd2d8d186fa4633f42aa
ihor <i.domnich2017@gmail.com>
2023-08-03 15:25:58 +0300

    Distribution of classes by different files.

---
+++ data.py
@@ -0,0 +1,7 @@
+class Data:
+    """Stores the usernames, emails, passwords, and API keys"""
+
+    email_pswd_list = [{"email": "craftsman94.test@gmail.com", "pswd": "craftsman94.test@gmail."[::-1]},
+                       {"email": "mindblow94@skiff.com", "pswd": "mindblow94@skiff."[::-1]},
+                       {"email": "mindblow94.0@skiff.com", "pswd": "mindblow94.0@skiff."[::-1]},
+                       {"email": "mindblow94.1@skiff.com", "pswd": "mindblow94.1@skiff."[::-1]}]
